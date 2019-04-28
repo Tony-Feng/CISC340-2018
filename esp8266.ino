5556a3568ee0d3df9eeb5a9be1cf7489198f95d8
Tony-Feng <43994607+Tony-Feng@users.noreply.github.com>
2019-04-28 15:07:25 -0400

    Update esp8266.ino

--- esp8266.ino
+++ esp8266.ino
@@ -1,3 +1,5 @@
+// Code for HUZZAH ESP8266 breakout board
+
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
@@ -16,23 +18,24 @@ void setup() {
 //{"timestamp:946699947,"heartrate":0,"temp":-20,"motion_x":9,"motion_y":19,"motion_z":29}
 //{"motion_x": 0.0, "timestamp": 946684817, "motion_y": -0.114914, "heartrate": 0, "motion_z": 9.15482, "temp": 23}
 void loop() {
-  if(WiFi.status() == WL_CONNECTED){   //Check WiFi connection status
+  if(WiFi.status() == WL_CONNECTED){ //Check WiFi connection status
     while (Serial.available() > 0) {
       data += char(Serial.read());
       delay(1);
     }
     if(data.length() > 0){
-      HTTPClient http;    //Declare object of class HTTPClient
+      HTTPClient http; //Declare object of class HTTPClient
       
-      http.begin("http://cisc340.canadacentral.cloudapp.azure.com:8080/store");      //Specify request destination
-      http.addHeader("Content-Type", "application/json");  //Specify content-type header
+      // The following address is no longer valid
+      http.begin("http://cisc340.canadacentral.cloudapp.azure.com:8080/store"); //Specify request destination
+      http.addHeader("Content-Type", "application/json"); //Specify content-type header
       
-      int httpCode = http.POST(data);   //Send the request
+      int httpCode = http.POST(data); //Send the request
       String payload = http.getString(); //Get the response payload
       
-      http.end();  //Close connection
+      http.end(); //Close connection
       data = "";
     }
   }
-  delay(1000);  //Send a request every 1 seconds
+  delay(1000); //Send a request every 1 seconds
 }
